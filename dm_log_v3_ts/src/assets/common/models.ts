1b004bf3a7db207bcda27d2446010e3083d74694
daimu <jinlin@wangdou-hbean.com>
2023-02-21 17:44:52 +0800

    Update models.ts

--- dm_log_v3_ts/src/assets/common/models.ts
+++ dm_log_v3_ts/src/assets/common/models.ts
@@ -9,6 +9,29 @@ class BaseEntity
 
     createDt: Date | null = null;
     updateDt: Date | null = null;
+
+
+    copy (obj: any) {
+        this.id = obj.id,
+        this.version = obj.version;
+        this.requestId = obj.requestId;
+        this.createBy = obj.createBy;
+        this.updateBy = obj.updateBy;
+
+        if (typeof(obj.createDt) == 'string') {
+            this.createDt = new Date(obj.createDt);
+        } else {
+            this.createDt = obj.createDt;
+        }
+
+        if (typeof(obj.updateDt) == 'string') {
+            this.updateDt = new Date(obj.updateDt);
+        } else {
+            this.updateDt = obj.updateDt;
+        }
+
+        return this;
+    }
 }
 
 export class LogDotaRun extends BaseEntity
@@ -16,5 +39,14 @@ export class LogDotaRun extends BaseEntity
     deviceId: string = "";
     groupId: string = "";
     isShop: boolean = false;
+
+    override copy(obj: any): this {
+        super.copy(obj);
+        this.deviceId = obj.deviceId;
+        this.groupId = obj.groupId;
+        this.isShop = obj.isShop;
+
+        return this;
+    }
 }
 
